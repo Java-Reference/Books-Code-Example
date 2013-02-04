5bbf37ceda005e67149545c7f441a1a466bd623f
Syed Sayem <syed@hotmail.com>
2013-02-03 22:37:58 -0500

    Initial commit

---
+++ JavaHowToProgram/src/main/java/com/sayem/ch28/UsingHugeInteger/build/generated/wsimport/client/com/deitel/jhtp7/ch28/hugeintegerclient/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://hugeinteger.ch28.jhtp7.deitel.com/")
+package com.sayem.ch28.UsingHugeInteger.build.generated.wsimport.client.com.deitel.jhtp7.ch28.hugeintegerclient;
